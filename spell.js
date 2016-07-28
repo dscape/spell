1e6bdad09b4b5b5d60cd6ad38561a447e6b72e42
alopezo <8TPFdVPnehjv4d>
2016-07-28 12:38:32 -0300

    Adding check method, to check a word without generating suggestions

--- spell.js
+++ spell.js
@@ -310,6 +310,24 @@ function spell_export(word) {
   return {corpus: dict};
 }
 
+/*
+ * check
+ *
+ * returns true or false for a given word if exists in dictionary
+ *
+ * e.g.
+ * spell.check('speling');
+ *
+ * @param {word:string:required}
+ *        the word you want to spell check
+ *
+ * @return {boolean}
+ *                 false
+ */
+function spell_check(word) {
+  return dict.hasOwnProperty(word);
+}
+
 return { reset       : spell_reset
        , load        : spell_load
        , "export"    : spell_export
@@ -319,6 +337,7 @@ return { reset       : spell_reset
        , remove_word : spell_remove_word
        , removeWord  : spell_remove_word  // alias
        , suggest     : spell_suggest
+       , check       : spell_check
        , lucky       : spell_lucky
        };
   };
